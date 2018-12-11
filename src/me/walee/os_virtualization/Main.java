531823b30f551c33b598bfed31ba9e1d3e21a136
Walee Ahmed <dev@walee.me>
2018-12-11 14:31:01 -0500

    cleaner bootstraping

--- src/me/walee/os_virtualization/Main.java
+++ src/me/walee/os_virtualization/Main.java
@@ -1,12 +1,8 @@
 package me.walee.os_virtualization;
 
 public class Main {
-    private  static void bootstrap() {
+    public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
         inputHandler.start();
-    }
-
-    public static void main(String[] args) {
-        bootstrap();
     }
 }
